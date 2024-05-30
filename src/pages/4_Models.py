d676a5c20f8a45ee754c207b982da5ce0d9cbe6a
chrappah <chrappahkwasi@gmail.com>
2024-05-30 00:58:09 +0000

    Create 4_Models.py

---
+++ src/pages/4_Models.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write("Hello,This is my first web app for a ML model")
+st.title ("Customer Churn Predictor")
+st.subheader("Page 4 : Predictor using the ML models created")
+x=st.text_input('Fovourite Movie?')
+st.write (f'Your movie is : {x}')
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
