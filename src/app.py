2d565905a7d6a585660d45f67a986de8b87a4d18
chrappah <chrappahkwasi@gmail.com>
2024-05-30 00:40:37 +0000

    Update app.py

--- src/app.py
+++ src/app.py
@@ -8,3 +8,6 @@ st.subheader("Page 3 : Predictor using the ML models created")
 st.caption("storage of all the predicted outcomes")
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+
+#streamlit run "c:/Users/chrap/OneDrive - ECG Ghana/Emmanuel Chrappah/Azubi Africa/git_hub_repos/streamlit_fundamentals/src/app.py"                               #
