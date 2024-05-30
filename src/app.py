3b7c73624d1b0d6e3685ad49711f2b17c0a80105
chrappah <chrappahkwasi@gmail.com>
2024-05-30 00:39:40 +0000

    beginning

---
+++ src/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.write("Hello,This is my first web app for a ML model")
+st.title ("Customer Churn Predictor")
+st.header("Page1:Readme on what the app does")
+st.markdown("Page2: sample of the data used in creating the model")
+st.subheader("Page 3 : Predictor using the ML models created")
+st.caption("storage of all the predicted outcomes")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
