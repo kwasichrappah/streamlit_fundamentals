7dc369bcdeeb7a046eb6b28c39fcb4d0e2c28c1d
chrappah <chrappahkwasi@gmail.com>
2024-06-02 01:09:40 +0000

    Update 5_predicted.py

--- src/pages/5_predicted.py
+++ src/pages/5_predicted.py
@@ -1,9 +1,42 @@
 import streamlit as st
+import pandas as pd
+import matplotlib as plt
+import sqlalchemy as sa
+import pyodbc  
+from dotenv import dotenv_values 
 
-st.write("Hello,This is my first web app for a ML model")
-st.title ("Customer Churn Predictor")
-st.caption("storage of all the predicted outcomes")
-x=st.text_input('Fovourite Movie?')
-st.write (f'Your movie is : {x}')
-st.code("x=2021")
-st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+
+st.title ("Data of Customers")
+st.subheader("This is a breakdown of customer attrition attributes")
+
+#Creation of Connection to Database
+
+
+
+# #Access protocols for the SQL Database
+# env_variables= dotenv_values('logins.env')
+# database= env_variables.get('database')
+# server = env_variables.get('server')
+# username = env_variables.get('username')
+# password = env_variables.get('password')
+
+# #Creation of Connection to Database
+# connection_string = f"DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};MARS_Connection=yes;MinProtocolVersion=TLSv1.2;"
+# #connection = pyodbc.connect(connection_string)
+# conn = sa.create_engine(connection_string)
+
+
+# #Querying SQL Database and reading the table into a dataframe
+# query = "SELECT * FROM LP2_Telco_churn_first_3000"
+
+# # Function to fetch data from the database
+# def get_data(query):
+#     with conn.connect() as connection:
+#         result = pd.read_sql(query, connection)
+#     return result
+
+
+
+# #sql_df= pd.read_sql(query, connection)
+
+
