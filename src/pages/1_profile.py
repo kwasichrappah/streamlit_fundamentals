2af98227b8028039fceea29ed48a5daba657fe49
chrappah <chrappahkwasi@gmail.com>
2024-06-04 17:51:45 +0000

    building_1

--- src/pages/1_profile.py
+++ src/pages/1_profile.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
 st.write("Hello,This is my first web app for a ML model")
-st.title ("Customer Churn Predictor")
+st.title ("This is Customer Churn Predictor App")
 st.header("Page1:Readme on what the app does")
--- src/pages/5_predicted.py
+++ src/pages/5_predicted.py
@@ -1,42 +1,22 @@
 import streamlit as st
 import pandas as pd
 import matplotlib as plt
-import sqlalchemy as sa
-import pyodbc  
-from dotenv import dotenv_values 
+
 
 
 st.title ("Data of Customers")
 st.subheader("This is a breakdown of customer attrition attributes")
 
-#Creation of Connection to Database
 
+df = pd.DataFrame(columns=['name','age','color'])
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+config = {
+    'name' : st.column_config.TextColumn('Full Name (required)', width='large', required=True),
+    'age' : st.column_config.NumberColumn('Age (years)', min_value=0, max_value=122),
+    'color' : st.column_config.SelectboxColumn('Favorite Color', options=colors)
+}
 
+result = st.data_editor(df, column_config = config, num_rows='dynamic')
 
-# #Access protocols for the SQL Database
-# env_variables= dotenv_values('logins.env')
-# database= env_variables.get('database')
-# server = env_variables.get('server')
-# username = env_variables.get('username')
-# password = env_variables.get('password')
-
-# #Creation of Connection to Database
-# connection_string = f"DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password};MARS_Connection=yes;MinProtocolVersion=TLSv1.2;"
-# #connection = pyodbc.connect(connection_string)
-# conn = sa.create_engine(connection_string)
-
-
-# #Querying SQL Database and reading the table into a dataframe
-# query = "SELECT * FROM LP2_Telco_churn_first_3000"
-
-# # Function to fetch data from the database
-# def get_data(query):
-#     with conn.connect() as connection:
-#         result = pd.read_sql(query, connection)
-#     return result
-
-
-
-# #sql_df= pd.read_sql(query, connection)
-
-
+if st.button('Get results'):
+    st.write(result)
