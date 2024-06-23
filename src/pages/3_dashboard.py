50988cec999b8462abff65051b76ded7d6033467
chrappah <chrappahkwasi@gmail.com>
2024-06-23 14:29:55 +0000

    Update 3_dashboard.py

--- src/pages/3_dashboard.py
+++ src/pages/3_dashboard.py
@@ -1,36 +1,45 @@
 import streamlit as st
 import time
-
-st.write("Hello,This is my first web app for a ML model")
-st.title ("Customer Churn Predictor")
-st.markdown("Page 2: Dashboards")
+import plotly.express as px
 
 
 
-'Starting a long computation...'
+def loader():
+  'Starting a long computation...'
+  # Add a placeholder
+  latest_iteration = st.empty()
+  bar = st.progress(0)
 
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
+  for i in range(100):
+    # Update the progress bar with each iteration.
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
 
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text(f'Iteration {i+1}')
-  bar.progress(i + 1)
-  time.sleep(0.1)
-
-'...and now we\'re done!'
-
-
+  '...and now we\'re done!'
 
 
-st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+def eda_dashboard():
+   st.markdown('### Exploratory Data AnalyDashboard')
 
-with st.expander("See explanation"):
-    st.write('''
-        The chart above shows some numbers I picked for you.
-        I rolled actual dice for these, so they're *guaranteed* to
-        be random.
-    ''')
-    st.image("https://static.streamlit.io/examples/dice.jpg")
-    
+def kpi_dashboard():
+   st.markdown ('### Key Performance Indicators')
+
+#st.bar_chart({"data": [1, 5, 2, 6, 2, 1]})
+
+
+if __name__ == '__main__':
+   
+   st.title("Dashboard")
+
+   col1,col2 = st.columns(2)
+   with col1:
+      pass
+   with col2:
+      st.selectbox('Select the type of Dashboard',options=['EDA','KPI'],key='dashboard_type')
+
+   if st.session_state['dashboard_type'] == 'EDA':
+    eda_dashboard()
+
+   else:
+    kpi_dashboard()
